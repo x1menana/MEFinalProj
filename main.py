3662626b37b01ff3352e04bdc4d47f4e830a3b4a
Ximena Salazar <ximenasalazar@Ximenas-MacBook-Pro.local>
2025-11-30 15:21:07 -0600

    finished training w 50 epochs

--- main.py
+++ main.py
@@ -7,25 +7,21 @@ print(cv2.__version__)
 yolo_model_path = 'yolo_models/yolov8n.pt'
 video_directories = ['30m_elevation_study_dkr', '100m_elevation_study_waterloo', 'drone_gimbal_gammarc', 'varying_elevation_study_dkr']
 ### Declare models here
-fine_tuned_model_path = 'yolo_models/best.pt'
+fine_tuned_model_path = 'runs/detect/train9/weights/best.pt'
 fine_tuned_model = YOLO(fine_tuned_model_path)
 print(f'fine tuned model names: {fine_tuned_model.names}')
-
-# yolo_model_path = '/Users/ximenasalazar/Desktop/application_prog/vehicle_detection/Vehicle-Detection/runs/train/exp12/weights/best.pt'
-# model = YOLO(yolo_model_path)
 
 def getColours(cls_num):
     random.seed(cls_num)
     return tuple(random.randint(0, 255) for _ in range(3))
 
 frame_count = 0
-min_conf = 0.7
-required_objects = ['Car']
+min_conf = 0.7 ### adjust lower/higher if needed
+required_objects = ['Vehicle']
 for dir in video_directories:
     for filename in os.listdir(dir):
         video_path = os.path.join(dir, filename)
         print(f'extracting frames from current video: {video_path}')
-
         # get vid capture
         videoCap = cv2.VideoCapture(video_path)
         if not videoCap.isOpened():
@@ -37,18 +33,15 @@ for dir in video_directories:
             if not ret:
                 break
             ### TODO HERE: feed in frame to model ###
-            # results = model.predict(source=frame, conf=0.1, verbose=True)  # lower conf for debugging
+            results = fine_tuned_model.track(frame, stream=True)
             # res = results[0]
             # print('num detections:', len(res.boxes))
-            results = fine_tuned_model.track(frame, stream=True)
             for result in results:
-                # print(f'result: {result}')
                 class_names = result.names
                 # print(f'class_names: {class_names}')
                 for box in result.boxes:
                     cls = int(box.cls[0])
                     class_name = class_names[cls]
-                    # print(f' curr class_name: {class_name}')
                     # if class_name not in required_objects: # if we dont detect a car or other req objects
                     #     continue
                     if box.conf[0] > min_conf:
@@ -59,7 +52,6 @@ for dir in video_directories:
                         cv2.putText(frame, f"{class_name} {conf:.2f}",
                                     (x1, max(y1 - 10, 20)), cv2.FONT_HERSHEY_SIMPLEX,
                                     0.6, colour, 2)
-                        
             cv2.imshow(f'video: {filename}', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
